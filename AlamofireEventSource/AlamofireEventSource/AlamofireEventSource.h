1caf702065cf6c8870e43f351dd691a16f7d0c1a
Nick Arner <nicholasarner@gmail.com>
2021-02-05 15:29:59 -0800

    Framework updates

---
+++ AlamofireEventSource/AlamofireEventSource/AlamofireEventSource.h
@@ -0,0 +1,18 @@
+//
+//  AlamofireEventSource.h
+//  AlamofireEventSource
+//
+//  Created by Nick Arner on 2/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlamofireEventSource.
+FOUNDATION_EXPORT double AlamofireEventSourceVersionNumber;
+
+//! Project version string for AlamofireEventSource.
+FOUNDATION_EXPORT const unsigned char AlamofireEventSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireEventSource/PublicHeader.h>
+
+
